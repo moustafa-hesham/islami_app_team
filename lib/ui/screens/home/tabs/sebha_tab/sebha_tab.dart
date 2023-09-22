b5738a9fbbd56b71119e46f7e6619747b45ee010
moustafa-hesham <113013570+moustafa-hesham@users.noreply.github.com>
2023-09-22 21:58:29 +0300

    Moustafa H-Sebha Page

--- lib/ui/screens/home/tabs/sebha_tab/sebha_tab.dart
+++ lib/ui/screens/home/tabs/sebha_tab/sebha_tab.dart
@@ -1,10 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:islami_sun_c9/ui/utils/app_colors.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
   const SebhaTab({super.key});
 
   @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  double angle = 0;
+  int sebhaCount = 0;
+  int index = 0;
+  List<String> tasabeehList = ['سبحان الله', "الحمد لله", " الله اكبر"];
+  @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.yellow,);
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              Image.asset('assets/images/head of seb7a.png'),
+              RotationTransition(
+                  turns: AlwaysStoppedAnimation(angle / 360),
+                  child: Image.asset('assets/images/body of seb7a.png')),
+            ],
+          ),
+          const Text('عدد التسبيحات'),
+          GestureDetector(
+            onTap: rotateImage,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: AppColors.primiary,
+                    ),
+                    width: 69,
+                    height: 81,
+                    child: Center(
+                      child: Text(
+                        sebhaCount.toString(),
+                        style: const TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.normal),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: AppColors.primiary,
+                  ),
+                  width: 137,
+                  height: 51,
+                  child: Center(
+                      child: Text(
+                    tasabeehList[index],
+                    style: const TextStyle(
+                        color: AppColors.white, fontWeight: FontWeight.normal),
+                  )),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void rotateImage() {
+    setState(() {
+      if (sebhaCount <= 32) {
+        sebhaCount++;
+      } else {
+        if (index < tasabeehList.length - 1) {
+          index++;
+          sebhaCount = 0;
+        } else {
+          index = 0;
+          sebhaCount = 0;
+        }
+      }
+      angle += 45.0;
+    });
   }
 }
